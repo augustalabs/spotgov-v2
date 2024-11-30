0e2796316943ae560e3639fcb4f746df9cebe8de
RicardoRocha27 <rocharicardo2799@gmail.com>
2024-11-30 17:02:16 +0000

    EDIT: fix schema in organizations

--- spotgov2/features/organizations/actions.ts
+++ spotgov2/features/organizations/actions.ts
@@ -1,12 +1,12 @@
 "use server";
 
 import { db } from "@/database/db";
-import { organizationsTable } from "@/database/schema";
 import * as z from "zod";
 import { createOrganizationSchema } from "./schemas";
+import { organizations } from "@/database/schema";
 
 export const createOrganization = async (
   organization: z.infer<typeof createOrganizationSchema>
 ) => {
-  return await db.insert(organizationsTable).values(organization).returning();
+  return await db.insert(organizations).values(organization).returning();
 };
