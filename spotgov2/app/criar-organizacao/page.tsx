5c812a2f09b361cce0aee1ce35389f02092533b2
RicardoRocha27 <rocharicardo2799@gmail.com>
2024-11-30 19:45:56 +0000

    EDIT: update schema and remove create-organization stuff

--- spotgov2/app/criar-organizacao/page.tsx
+++
@@ -1,7 +0,0 @@
-import CreateOrganizationForm from "@/features/organizations/components/create-organization-form";
-
-const CreateOrganizationPage = () => {
-  return <CreateOrganizationForm />;
-};
-
-export default CreateOrganizationPage;
