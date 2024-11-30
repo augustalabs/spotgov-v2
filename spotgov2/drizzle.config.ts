517b0e228004b6e4220ada78c5c408067a104b11
RicardoRocha27 <rocharicardo2799@gmail.com>
2024-11-30 17:03:19 +0000

    EDIT: move migrations to database folder

--- spotgov2/drizzle.config.ts
+++ spotgov2/drizzle.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'drizzle-kit';
+import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: './database/schema.ts',
-  out: './migrations',
-  dialect: 'postgresql',
+  schema: "./database/schema.ts",
+  out: "./database/migrations",
+  dialect: "postgresql",
   dbCredentials: {
     url: process.env.SUPABASE_CONNECTION_STRING!,
   },
