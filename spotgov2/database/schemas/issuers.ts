32f25c26b6a0e2db8e6bbee6156a122f9e5a94fa
Tiago Pereira <tiagorp2004@gmail.com>
2024-12-01 02:58:34 +0000

    EDIT: small schema change

--- spotgov2/database/schemas/issuers.ts
+++ spotgov2/database/schemas/issuers.ts
@@ -4,6 +4,7 @@ import { numeric, pgTable, text } from "drizzle-orm/pg-core";
 const issuers = pgTable("issuers_list", {
   nif: text("nif").primaryKey(),
   name: text("name"),
+  altName: text("alt_name"),
   totalValueClosed: numeric("total_value_closed"),
 });
 
