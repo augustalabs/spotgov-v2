ef5545a1bb982f7e457b5f3ecd01f88de8198869
RicardoRocha27 <rocharicardo2799@gmail.com>
2024-12-02 14:36:52 +0000

    EDIT: add pages for other routes to avoid 404

---
+++ spotgov2/app/(private)/pesquisas-favoritas/page.tsx
@@ -0,0 +1,3 @@
+export default function FavoriteSearchesPage() {
+  return <div>Favorite Searches Page</div>;
+}
--- spotgov2/app/auth/registar/page.tsx
+++ spotgov2/app/auth/registar/page.tsx
@@ -1,5 +1,5 @@
 import SignUpForm from "@/features/auth/components/sign-up-form";
 
-export default function LoginPage() {
+export default function SignUpPage() {
   return <SignUpForm />;
 }
