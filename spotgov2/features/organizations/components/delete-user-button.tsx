ed0667ebf9b535d607153a675833a907edac4845
RicardoRocha27 <rocharicardo2799@gmail.com>
2024-12-10 16:09:37 +0000

    EDIT: update DestructiveActionDialog to deal with user removal

--- spotgov2/features/organizations/components/delete-user-button.tsx
+++ spotgov2/features/organizations/components/delete-user-button.tsx
@@ -1,3 +1,4 @@
+import DestructiveActionDialog from "@/components/destructive-action-dialog";
 import { Button } from "@/components/ui/button";
 import {
   Tooltip,
@@ -8,42 +9,53 @@ import deleteUserMutation from "@/mutations/delete-user-mutation";
 import { useCurrentOrganizationStore } from "@/stores/current-organization-store";
 import { useMutation } from "@tanstack/react-query";
 import { Trash } from "lucide-react";
+import { useState } from "react";
 import { toast } from "sonner";
 
 const DeleteUserButton = ({ userId }: { userId: string }) => {
   const { currentOrganization } = useCurrentOrganizationStore();
 
   const mutation = useMutation(
-    deleteUserMutation(currentOrganization?.organizationId as string)
+    deleteUserMutation(currentOrganization?.organizationId as string),
   );
 
   const handleDelete = async () => {
     try {
       const res = await mutation.mutateAsync({ userId });
-
       if (res.success) {
         toast.success("Utilizador removido com sucesso.");
       } else {
         toast.error(
-          "Ocorreu um erro ao remover o utilizador. Por favor, tente novamente."
+          "Ocorreu um erro ao remover o utilizador. Por favor, tente novamente.",
         );
       }
+
+      return res.success;
     } catch {
       toast.error(
-        "Ocorreu um erro ao remover o utilizador. Por favor, tente novamente."
+        "Ocorreu um erro ao remover o utilizador. Por favor, tente novamente.",
       );
+      return false;
     }
   };
 
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   return (
-    <Tooltip>
-      <TooltipTrigger asChild>
-        <Button variant="destructive" size="sm" onClick={handleDelete}>
-          <Trash size={16} />
-        </Button>
-      </TooltipTrigger>
-      <TooltipContent>Remover utilizador</TooltipContent>
-    </Tooltip>
+    <DestructiveActionDialog
+      isOpen={isOpen}
+      setIsOpen={setIsOpen}
+      action={handleDelete}
+    >
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Button variant="destructive" size="sm">
+            <Trash size={16} />
+          </Button>
+        </TooltipTrigger>
+        <TooltipContent>Remover utilizador</TooltipContent>
+      </Tooltip>
+    </DestructiveActionDialog>
   );
 };
 
