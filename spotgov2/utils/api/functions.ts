5b68ff26205b184c17401e8a7999a0f180543d4b
Tiago Pereira <tiagorp2004@gmail.com>
2024-12-16 15:27:31 +0000

    FEAT: is_super_admin fields on register

--- spotgov2/utils/api/functions.ts
+++ spotgov2/utils/api/functions.ts
@@ -18,6 +18,21 @@ export async function get<T>({ url }: ApiRequestOptions): Promise<T> {
   return data as T;
 }
 
+export async function getServer<T>({ url }: ApiRequestOptions): Promise<T> {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/${url}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!res.ok) throw new Error("Failed to fetch data.");
+
+  const data = await res.json();
+
+  return data as T;
+}
+
 export async function post<T>({ url, body }: ApiRequestOptions): Promise<T> {
   const res = await fetch(`/api/${url}`, {
     method: "POST",
