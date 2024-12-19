e44381f99205326f592c3d4dbdfd937e98177433
RicardoRocha27 <rocharicardo2799@gmail.com>
2024-12-19 14:40:58 +0000

    EDIT: change radar header

--- spotgov2/app/(private)/radar/page.tsx
+++ spotgov2/app/(private)/radar/page.tsx
@@ -1,11 +1,11 @@
 import Header from "@/components/header";
 import CustomTable from "@/features/radar/components/custom-table";
-import { SAVED_CONTESTS_ROUTE } from "@/routes";
+import { CONTESTS_RADAR_ROUTE } from "@/routes";
 
 export default function FavoriteSearchesPage() {
   return (
     <section>
-      <Header title={SAVED_CONTESTS_ROUTE.label} />
+      <Header title={CONTESTS_RADAR_ROUTE.label} />
       <CustomTable />
     </section>
   );
