28004fcee1f31a3d55f4f45fce1e0b5f6e8fe842
RicardoRocha27 <rocharicardo2799@gmail.com>
2024-12-20 21:32:06 +0000

    EDIT: improve UX

--- spotgov2/features/radar/components/custom-columns/date-column.tsx
+++ spotgov2/features/radar/components/custom-columns/date-column.tsx
@@ -12,6 +12,7 @@ import { Calendar as CalendarIcon } from "lucide-react";
 import { useState } from "react";
 import {
   addColumnValueMutation,
+  deleteColumnValueMutation,
   updateColumnValueMutation,
 } from "../../services";
 import { toast } from "sonner";
@@ -37,31 +38,44 @@ const DateColumn = ({ value, fieldId, contractId }: DateColumnProps) => {
     addColumnValueMutation(currentOrganization?.organizationId as string),
   );
 
+  const deleteValueMutation = useMutation(
+    deleteColumnValueMutation(currentOrganization?.organizationId as string),
+  );
+
   const handleValueEdit = async (v: Date | undefined) => {
     try {
-      if (!v) return;
-
-      // Optimistic update
-      setNewValue(v.toISOString());
-
       let res;
 
-      if (!value) {
-        res = await addValueMutation.mutateAsync({
-          value: v.toISOString(),
+      if (!v) {
+        res = await deleteValueMutation.mutateAsync({
           fieldId,
           contractId,
         });
       } else {
-        res = await updateValuemutation.mutateAsync({
-          value: v.toISOString(),
-          fieldId,
-          contractId,
-        });
+        // Optimistic update
+        setNewValue(v.toISOString());
+
+        if (!value) {
+          res = await addValueMutation.mutateAsync({
+            value: v.toISOString(),
+            fieldId,
+            contractId,
+          });
+        } else {
+          res = await updateValuemutation.mutateAsync({
+            value: v.toISOString(),
+            fieldId,
+            contractId,
+          });
+        }
       }
 
       if (res.success) {
-        toast.success("Valor atualizado com sucesso.");
+        if (!v) {
+          toast.success("Valor removido com sucesso.");
+        } else {
+          toast.success("Valor atualizado com sucesso.");
+        }
       } else {
         toast.error("Erro ao atualizar valor. Por favor, tente novamente.");
       }
